623a47f9e3522ab62b2a00351073d9a444f420cc
SardorAKayumov <sardor.zeroz@gmail.com>
2022-09-18 14:41:03 +0500

    done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        //I don't really get the task, so I will just write the formula into a variable
+        String formula = "(9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2)";
     }
 }
